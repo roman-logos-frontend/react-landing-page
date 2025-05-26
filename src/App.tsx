e9594d0be8d9dca88921f3a686a1f9d131efdcfb
roman-logos-frontend <romanst235@gmail.com>
2025-05-26 18:33:48 +0300

    Landing Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import React from 'react';
 import './App.scss';
+import { ComponentHeader } from './components/Header';
+import { ComponentFooter } from './components/Footer';
+import { Outlet } from 'react-router-dom';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
-  );
-};
+export const App = () => (
+  <div className="App">
+    <ComponentHeader />
+    <main>
+      <Outlet />
+    </main>
+    <ComponentFooter />
+  </div>
+);
